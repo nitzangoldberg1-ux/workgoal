75b5dd7262a685d43c283e2cd489dbe52df98d3b
nitzangoldberg1-ux <nitzangoldberg1@gmail.com>
2026-08-30 17:12:30 +0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-// Paste your Firebase Web App config here.
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyB9Z_LrfeKKdCn0ddvJCM0Fc6ON1Vdg_98",
+  authDomain: "workgoal-efa37.firebaseapp.com",
+  projectId: "workgoal-efa37",
+  storageBucket: "workgoal-efa37.firebasestorage.app",
+  messagingSenderId: "1008676077963",
+  appId: "1:1008676077963:web:e8595fa198b8f339a3af8e",
+  measurementId: "G-P770K2VE9R"
 };
-
 // Email allowed to use the admin screen. Must match Firestore rules.
 export const ADMIN_EMAIL = "your-admin@email.com";
